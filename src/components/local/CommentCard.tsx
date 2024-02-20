6ea6cb13014ccfb93c804befdb9f8f8f137f363b
Jace-Tech <alexjace151@gmail.com>
2024-02-20 06:25:21 -0800

    fix: updated the comment card

--- src/components/local/CommentCard.tsx
+++ src/components/local/CommentCard.tsx
@@ -1,5 +1,5 @@
 
-import { Avatar, Button, Collapse, Flex, HStack, Heading, Stack, Text, useDisclosure } from '@chakra-ui/react'
+import { Avatar, Button, Collapse, Flex, HStack, Heading, Spacer, Stack, Text, useDisclosure } from '@chakra-ui/react'
 import React from 'react'
 import useColorMode from '../../hooks/useColorMode'
 import dayjs from 'dayjs';
@@ -11,17 +11,17 @@ dayjs.extend(relativeTime)
 
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface CommentCardProps extends CommentReply { 
+interface CommentCardProps extends CommentReply {
     setIsReplying?: (data: CommentReply) => void;
 }
 
 const CommentCard: React.FC<CommentCardProps> = (prop) => {
-    const { setIsReplying} = prop
+    const { setIsReplying } = prop
     const { colors } = useColorMode()
     const { isOpen, onToggle } = useDisclosure()
     return (
         <Stack spacing={0} mb={2}>
-            <UserCommentComponent 
+            <UserCommentComponent
                 comment={prop.comment}
                 date={prop.date}
                 prop={prop}
@@ -30,12 +30,12 @@ const CommentCard: React.FC<CommentCardProps> = (prop) => {
             />
             <Collapse in={isOpen} animateOpacity>
                 <Stack ml={4} pl={2} py={4} borderLeft={`1px solid ${colors.DIVIDER}`}>
-                    { prop?.reply && prop.reply.map(reply => (
-                        <UserCommentComponent date={reply.date} key={`reply-${reply._id}`} comment={reply.reply} user={reply.user}  />
-                    )) }
+                    {prop?.reply && prop.reply.map(reply => (
+                        <UserCommentComponent date={reply.date} key={`reply-${reply._id}`} comment={reply.reply} user={reply.user} />
+                    ))}
                 </Stack>
             </Collapse>
-            { prop?.reply && prop.reply.length ? <Button alignSelf={"flex-start"} onClick={onToggle} variant={"link"} size={"xs"} colorScheme='gray'>{!isOpen ? "View" : "Hide"} replies ({ prop?.reply.length })</Button> : null }
+            {prop?.reply && prop.reply.length ? <Button alignSelf={"flex-start"} onClick={onToggle} variant={"link"} size={"xs"} colorScheme='gray'>{!isOpen ? "View" : "Hide"} replies ({prop?.reply.length})</Button> : null}
         </Stack>
     )
 }
@@ -56,18 +56,21 @@ interface UserCommentComponentProp {
 const UserCommentComponent: React.FC<UserCommentComponentProp> = ({ user, comment, date, setIsReplying, prop }) => {
     const { colors, hoverColor } = useColorMode()
     return (
-        <HStack spacing={2} px={2} alignItems={"start"} py={2} bg={hoverColor}>
-            <Avatar size={'md'} as={Link} to={`/profile/${user?.username}`} borderWidth={1} borderStyle={"solid"} borderColor={colors.DIVIDER} src={user?.profileImage} name={user?.musicName || user?.username} />
-            <HStack alignItems={"center"} flex={1}>
-                <Stack spacing={1} flex={1}>
+        <Stack spacing={2} px={2} py={2} bg={hoverColor} borderRadius={"md"}>
+            <HStack spacing={2} alignItems={"center"}>
+                <Avatar size={'md'} as={Link} to={`/profile/${user?.username}`} borderWidth={1} borderStyle={"solid"} borderColor={colors.DIVIDER} src={user?.profileImage} name={user?.musicName || user?.username} />
+                <Stack spacing={0} alignItems={"flex-start"} flex={1}>
                     <Flex as={Link} to={`/profile/${user?.username}`} alignItems={'center'} gap={3}>
                         <Heading color={colors.TEXT_WHITE} size={"xs"}>{user?.musicName || user?.username}</Heading>
-                        <Text color={colors.TEXT_GRAY} fontSize={'xs'}>{dayjs(date).fromNow()}</Text>
                     </Flex>
-                    <Text color={colors.TEXT_DARK} fontSize={"sm"}>{comment}</Text>
+                    <Text color={colors.TEXT_GRAY} fontSize={'xs'}>{dayjs(date).fromNow()}</Text>
                 </Stack>
+            </HStack>
+            <Text color={colors.TEXT_DARK} fontSize={"sm"}>{comment}</Text>
+            <HStack justifyContent={"flex-end"}>
+
                 {setIsReplying && <CustomButton onClick={() => setIsReplying(prop)} size={"sm"} bg={"transparent"} _hover={{ color: colors.PRIMARY_COLOR }} color={colors.PRIMARY_COLOR} variant={"link"}>Reply</CustomButton>}
             </HStack>
-        </HStack> 
+        </Stack>
     )
 }
